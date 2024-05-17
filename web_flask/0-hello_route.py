22e95cd3338db8b931e8ed6309d03e96ad17681f
Keegan <keeganjoubert@protonmail.com>
2024-05-17 04:33:26 -0700

    AirBnb

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-from flask import Flask
+"""
+Starts a Flask web application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def index():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
