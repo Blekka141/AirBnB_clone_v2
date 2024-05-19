a8fd7f82c544baf8572460ff4fdd9f8c48e18c45
Keegan <keeganjoubert@protonmail.com>
2024-05-19 12:45:17 -0700

    List of States

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,6 @@ starts a Flask web application
 from flask import Flask, render_template
 from models import *
 from models import storage
-
 app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
